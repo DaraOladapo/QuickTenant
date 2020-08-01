ae9207bddd00275c64042a439e645704a672a9dc
Dara Oladapo <daraoladapo@outlook.com>
2020-08-01 14:21:26 +0100

    Add project files.

---
+++ QuickTenant/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace QuickTenant.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
